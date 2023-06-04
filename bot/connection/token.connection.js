c4492570491b1397a662f90a989aea5df14f4eb7
Romanlovalova <romanzakharich@gmail.com>
2023-06-04 21:11:07 +0800

    add user lite lust

--- bot/connection/token.connection.js
+++ bot/connection/token.connection.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const { Telegraf } = require("telegraf");
 
-module.exports = new Telegraf('596354294:AAHfSI_17rvFq-wAj-WL-dBOtcaxjwgNJUc');
-//6178032159:AAEtM_2OzIjWZqs9OJrQiqcjjqYTXdWRc_E
+module.exports = new Telegraf('');
+//
 //
 
